b5a97ff85683b2109c03d8b5833b30e3dcbfd78e
Artem Golovin <artempotapkin96@gmail.com>
2021-04-26 14:03:17 +0300

    Артем: При нажатии на кнопку Рассчитать должен выводиться объект с ключами и значениями ввода 2.0

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ let form = document.querySelector('.userForm');
 let calculateButton = document.querySelector('.calculate-button');
 let targetName = document.querySelector('.target-name');
 let requiredAmount = document.querySelector('.required-amount');
-let depositTerm = document.querySelector('.deposit-term');
+let depositPercentage = document.querySelector('.deposit-percentage');
 let startingAmount = document.querySelector('.starting-amount');
 class CalculateResultsObject {
-  constructor (name, amount, deposit, startamount) {
+  constructor(name, amount, persents, startamount) {
     this.name = name
     this.amount = amount;
-    this.deposit = deposit;
+    this.persents = persents;
     this.startamount = startamount;
   }
 }
 
 calculateButton.addEventListener('click', (e) => {
   e.preventDefault();
-  let objOfResult = new CalculateResultsObject(targetName.value, requiredAmount.value, depositTerm.value, startingAmount.value);
+  let objOfResult = new CalculateResultsObject(targetName.value, requiredAmount.value, depositPercentage.value, startingAmount.value);
   console.log(objOfResult);
 })
