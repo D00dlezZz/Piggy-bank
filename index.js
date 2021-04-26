ed5ee1b18429d46685fae076c5876171ddfe06c1
Mikhail <mihail.kuznetsov.99@mail.ru>
2021-04-26 15:36:21 +0300

    project

--- index.js
+++ index.js
@@ -3,28 +3,31 @@
 
 let form = document.querySelector('.userForm');
 let calculateButton = document.querySelector('.calculate-button');
-let targetName = document.querySelector('.target-name');
-let requiredAmount = document.querySelector('.required-amount');
-let depositPercentage = document.querySelector('.deposit-percentage');
+let targetName = document.querySelector('.target-text');
+let requiredAmount = document.querySelector('.required-amount-text');
+let dateRequire = document.querySelector('.term-of-deposit')
+let depositPercentage = document.querySelector('.percent');
 let startingAmount = document.querySelector('.starting-amount');
 
 class CalculateResultsObject {
-  constructor(name, amount, percents, startamount) {
-    this.name = name
-    this.amount = amount;
-    this.percents = percents;
-    this.startamount = startamount;
+  constructor(name, amount, percents, startamount, date) {
+    this.name = name,
+    this.amount = amount,
+    this.percents = percents,
+    this.startamount = startamount,
+    this.date = date
   }
 }
 
 calculateButton.addEventListener('click', (element) => {
   element.preventDefault();
-  let objOfResult = new CalculateResultsObject(targetName.value, requiredAmount.value, depositPercentage.value, startingAmount.value);
+  let objOfResult = new CalculateResultsObject(targetName.value, requiredAmount.value, depositPercentage.value, startingAmount.value, dateRequire.value);
   const newSection = document.createElement('ul');
   newSection.className = 'target-card';
-  newSection.innerHTML = `<li class="item"><p>Размер платежа</p><input value = ""></li>
+  newSection.innerHTML = `<li class="item"><p>Размер платежа</p><input value =""></li>
                           <li class="item"><p>Название цели</p><input value = "${objOfResult.name}"></li>
                           <li class="item"><p>Требуемая сумма</p><input value = "${objOfResult.amount}"></li>
+                          <li class="item"><p>Срок выплаты</p><input value = "${objOfResult.date}"></li>
                           <li class="item"><p>Стартовая сумма</p><input value = "${objOfResult.startamount}"></li>
                           <li class="item"><p>Процент по вкладу</p><input value = "${objOfResult.percents}"></li>`;
   document.querySelector('.newSection').append(newSection);
